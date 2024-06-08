b02f137dc87374a480db53cf8e4a25a737bca322
Paredes Micaela <miparedes@frba.utn.edu.ar>
2024-06-07 21:36:46 -0300

    Update Usuario.java

--- src/main/java/org/example/Prendas/Usuario.java
+++ src/main/java/org/example/Prendas/Usuario.java
@@ -3,6 +3,9 @@ package org.example.Prendas;
 public class Usuario {
     private Sugerencia sugerenciaDiaria;
 
+    //estado comunicacional es guardar un estado pero no retornarlo porque nadie lo usa.
+    //hay empleados en el sistema pero no es un concepto, poner objetos para que hagan una accion porque si esta mal.
+
     public void actualizarSugerenciaDiaria(Sugerencia sugerencia) {
         sugerenciaDiaria = sugerencia;
     }
